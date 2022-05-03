ffb7b417ab85f7c7cd04cb3b91b627b80f8c6217
code-manavkasare <code.manavkasare@gmail.com>
2022-05-03 12:31:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [type, setType] = useState(null);
+
+  const handleSubmit = async () => {
+    console.log("making api call...");
+    const response = await fetch(
+      `http://127.0.0.1:5000/persona?username=${username}`
+    );
+    console.log("response", response);
+    const data = await response.json();
+    console.log("data", data);
+    if (data.data) return setType(data.data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        name="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Get MBTI personality</button>
+      {type && <h2>Your personality type is {type}</h2>}
     </div>
   );
 }
